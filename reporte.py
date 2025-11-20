8000f7219aa9e2cc9b8684fddd860f5919f39118
Proyecto_I+D <roberto.hernandezap@alumno.buap.mx>
2025-11-20 00:34:10 -0600

    Sospechoso

--- reporte.py
+++ reporte.py
@@ -3,11 +3,15 @@ import cv2
 class Reporte:
 
     @staticmethod
+
     def construir_reporte(elapsed, analyzer):
         total_no = analyzer.total_no_atention
+
         porcentaje_no = (total_no / elapsed) * 100.0 if elapsed > 0 else 0.0
 
         b = analyzer.no_attention_breakdown
+        # Determinar si hay comportamiento sospechoso
+        sospechoso = "SÍ" if porcentaje_no > 40.0 else "NO"
 
         reporte = (
             f"--- Reporte de Atención ---\n\n"
@@ -19,8 +23,8 @@ class Reporte:
             f" - Giro hacia arriba: {b['up']:.2f} s\n"
             f" - Giro hacia abajo: {b['down']:.2f} s\n"
             f" - Pérdida del rostro: {b['lost_roi']:.2f} s\n"
-            f" - Cambio de ventana: {b['focus_change']:.2f} s\n"
-            
+            f" - Cambio de ventana: {b['focus_change']:.2f} s\n\n"
+            f"Comportamiento sospechoso: {sospechoso} (más del 40% sin atención)\n"
         )
 
         return reporte
